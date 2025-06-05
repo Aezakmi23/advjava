b207d89f96495e93c8953475d87a6d3bb6d8422e
Sangeeta Singh <Sangeeta Singh@Aezak>
2025-06-06 00:44:26 +0530

    assignment 1

--- JDBC/src/dac/A1.java
+++ JDBC/src/dac/A1.java
@@ -2,19 +2,33 @@ package dac;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class A1 {
 
 	public static void main(String[] args) {
 	
 			try {
-				Connection db = DriverManager.getConnection("jdbc:mysql://localhost:3306/advjava","root", "2303");
+				Connection dbconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/advjava","root", "2303");
+				
+				Statement st = dbconnection.createStatement();
+				
+				ResultSet r = st.executeQuery("select * from userdetail");
+				while (r.next()) {
+					System.out.println(r.getString(1));
+					System.out.println(r.getString(2));
+					System.out.println(r.getString(3));
+					System.out.println(r.getString(4));
+					System.out.println(r.getString(5));
+					System.out.println("--------------------------");
+					
+				}
+				
 			} catch (SQLException e) {
 				e.printStackTrace();
-			}
-			
-
+			} 
 	}
 
 }
